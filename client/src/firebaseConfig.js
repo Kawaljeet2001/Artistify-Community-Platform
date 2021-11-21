1dfba5fe225eb3a141f0ad0958df79ec1514637a
Kawaljeet Singh Batra <batrakawaljeetsingh@gmail.com>
2021-11-21 14:29:09 +0530

    created env variables

--- client/src/firebaseConfig.js
+++ client/src/firebaseConfig.js
@@ -2,15 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getStorage , ref } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA6WJNjBeS1XMN9jxzOULqxAA5MXkfIRf8",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: "artistifycommunity.firebaseapp.com",
     projectId: "artistifycommunity",
     storageBucket: "artistifycommunity.appspot.com",
-    messagingSenderId: "1028549383732",
-    appId: "1:1028549383732:web:08ffe6132e01a8c6779c61"
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
   };
 
   const firebaseApp = initializeApp(firebaseConfig);
 
   export const storage = getStorage(firebaseApp);
   export const avatarStorageRef = ref(storage , 'avatar');
+  
